8be46df63c2665a26eda1503126c8c6d6b5f16f9
Tram <s216535@student.dtu.dk>
2022-03-21 17:10:31 +0000

    test make_codons + changes in description file

--- R/data.R
+++ R/data.R
@@ -2,7 +2,6 @@
 #'
 #' Degenerate genetic code
 #'
-#' @format
 #'
 #' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
 "codon_table"
--- R/mRNA.R
+++ R/mRNA.R
@@ -1,4 +1,13 @@
+#' Function2_make_codons
+#'
+#' @param dna_seq
+#'
+#' @return
+#' @export
+#'
+#' @examples
 mRNA <- function(dna_seq){
   T_to_U <- gsub("T", "U", dna_seq)
   return(T_to_U)
 }
+
