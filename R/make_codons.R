3569a67ee1dd97f4f6ec4b1df0ace67ae0d555e0
Tram <s216535@student.dtu.dk>
2022-03-21 12:25:31 +0000

    make_codons test

--- R/make_codons.R
+++ R/make_codons.R
@@ -1,6 +1,15 @@
+#' Title
+#'
+#' @param mRNA
+#' @param start
+#'
+#' @return
+#' @export
+#'
+#' @examples
 make_codons <- function(mRNA, start = 1){
-  mrna_seq <- nchar(mRNA)
-  codons <- substring(mrna_seq,
+  seq_length <- nchar(mRNA)
+  codons <- substring(mRNA,
                       first = seq(from = start, to = seq_length-3+1, by = 3),
                       last = seq(from = 3+start-1, to = seq_length, by = 3))
   return(codons)
