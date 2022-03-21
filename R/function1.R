0cec629d6a9c794e66027d471899f93343dd0f76
Linesandvad <s184339@student.dtu.dk>
2022-03-21 09:21:36 +0000

    function1 "generate_dna"

---
+++ R/function1.R
@@ -0,0 +1,17 @@
+#function takes in a given length (double)
+#generates a random sample of bases in dna
+#and pastes them together into one string,
+#which is thus a randomly generated dna sequence
+
+#name_me1 = generate_dna
+#name_me2 = dna_length
+#name_me3 = dna_samp_rand 
+#name_me4 = dna_seq
+
+generate_dna <- function(dna_length){
+  dna_samp_rand <- sample(c("A", "T", "G", "C"), 
+                     size = dna_length, 
+                     replace = TRUE)
+  dna_seq <- paste0(dna_samp_rand, collapse = "")
+  return(dna_seq)
+}
