c90f483f844061a4859fdb12e5a84cbdcf2007e5
Linesandvad <s184339@student.dtu.dk>
2022-03-21 09:50:27 +0000

    function3 and 4

---
+++ R/function3.R
@@ -0,0 +1,12 @@
+name_me1 <- function(mRNA, start = 1){
+  mrna_seq <- nchar(mRNA)
+  codons <- substring(mrna_seq,
+                      first = seq(from = start, to = seq_length-3+1, by = 3),
+                      last = seq(from = 3+start-1, to = seq_length, by = 3))
+  return(codons)
+}
+
+# example to understand substring
+# seq_length = 12 (4 codons)
+# first -> 1..10
+# last -> 3..12
---
+++ R/function4.R
@@ -0,0 +1,4 @@
+translation <- function(codons){
+  aa_code <- paste0(codon_table[codons], collapse = "")
+  return(aa_code)
+}
